84d8a27601802e1a78d8eb42d35b859951e6392a
Irene Knapp <irene.knapp@icloud.com>
2013-03-02 10:07:23 -0500

    Some stuff to move it around.

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,6 +1,63 @@
 module Main (main) where
 
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Prelude as IO
+import qualified System.Environment as IO
+
+import Control.Monad
+import Data.Conduit
+import Data.Conduit.Text
+
+import Eyeshadow.Conduit
+
+
+import Prelude
+  (IO,
+   show,
+   ($))
+
+
+data SourcePosition =
+  SourcePosition {
+      sourcePositionByteOffset :: Int,
+      sourcePositionCharacterOffset :: Int,
+      sourcePositionLine :: Int,
+      sourcePositionColumn :: Int
+    }
+
+data SourceSpan =
+  SourceSpan {
+      sourceSpanStart :: SourcePosition,
+      sourceSpanEnd :: SourcePosition
+    }
+
+data Diagnostic =
+  Diagnostic {
+      diagnosticHeadline :: T.Text,
+      diagnosticDescription :: T.Text,
+      diagnosticDetails :: [(T.Text, SourceSpan)]
+    }
+
+
 
 main :: IO ()
 main = do
-  putStrLn "Hello, stdout!"
+  arguments <- IO.getArgs
+  case arguments of
+    [inputFilePath] -> process inputFilePath
+    _ -> IO.putStrLn $ "Usage: eyeshadow input.hue"
+
+
+process :: IO.FilePath -> IO ()
+process sourcePath = do
+  readFile sourcePath $$ do
+    let loop = do
+          maybeValue <- await
+          case maybeValue of
+            Nothing -> return ()
+            Just (c, sourcePosition) -> do
+              putStrLn $ show (c, sourcePosition)
+              loop
+    loop
